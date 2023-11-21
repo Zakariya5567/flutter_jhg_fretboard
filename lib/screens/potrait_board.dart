9fd685857964496e9a009c53a764f6f9c6107655
Muhammad zakariya <mdzakariya333@gmail.com>
2023-11-21 14:47:19 +0500

    get started button allaw

--- lib/screens/potrait_board.dart
+++ lib/screens/potrait_board.dart
@@ -80,41 +80,41 @@ class PortraitBoard extends StatelessWidget {
                ),
 
               // //TIMER , STOPWATCH , ROTATE ICON
-              Padding(
-                padding:  EdgeInsets.only(
-                  left: width * 0.08,
-                  right: width * 0.08,),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ButtonIcon(
-                        icon: Images.iconTimer,
-                        width: height*0.03,
-                        height: height*0.03,
-                        onTap: (){}),
-
-
-                    Text(
-                      "01:34",
-                      style: TextStyle(
-                        fontFamily: AppConstant.sansFont,
-                        color: AppColors.whitePrimary,
-                        fontSize: 40,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-
-
-                    ButtonIcon(
-                        icon: Images.iconRotate,
-                        width: height*0.05,
-                        height: height*0.04,
-                        onTap: (){
-                          controller.toggleOrientation();
-                        })
-                  ],
-                ),
-              ),
+              // Padding(
+              //   padding:  EdgeInsets.only(
+              //     left: width * 0.08,
+              //     right: width * 0.08,),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //     children: [
+              //       ButtonIcon(
+              //           icon: Images.iconTimer,
+              //           width: height*0.03,
+              //           height: height*0.03,
+              //           onTap: (){}),
+              //
+              //
+              //       Text(
+              //         "01:34",
+              //         style: TextStyle(
+              //           fontFamily: AppConstant.sansFont,
+              //           color: AppColors.whitePrimary,
+              //           fontSize: 40,
+              //           fontWeight: FontWeight.w700,
+              //         ),
+              //       ),
+              //
+              //
+              //       ButtonIcon(
+              //           icon: Images.iconRotate,
+              //           width: height*0.05,
+              //           height: height*0.04,
+              //           onTap: (){
+              //             controller.toggleOrientation();
+              //           })
+              //     ],
+              //   ),
+              // ),
               //
               //
               // //SCORE
@@ -130,48 +130,48 @@ class PortraitBoard extends StatelessWidget {
               //         fontWeight: FontWeight.w600,
               //       ),
               //     ),
-              //     Text(
-              //       controller.highlightNode??"",
-              //       style: TextStyle(
-              //         fontFamily: AppConstant.sansFont,
-              //         color: AppColors.whitePrimary,
-              //         fontSize: 16,
-              //         fontWeight: FontWeight.w600,
-              //       ),
-              //     ),
+                  Text(
+                    controller.highlightNode??"",
+                    style: TextStyle(
+                      fontFamily: AppConstant.sansFont,
+                      color: AppColors.whitePrimary,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                // ],
              // ),
 
-            //  SPACER
-            //   SizedBox(
-            //     height: height*0.02,
-            //   ),
+            // SPACER
+              SizedBox(
+                height: height*0.02,
+              ),
 
-             // START BUTTON
-             //  InkWell(
-             //    onTap: () {
-             //      controller.startTheGame();
-             //    },
-             //    child: Center(
-             //      child: Container(
-             //        height: height * 0.07,
-             //        width: width * 0.8,
-             //        alignment: Alignment.center,
-             //        decoration: BoxDecoration(
-             //            color: AppColors.redPrimary,
-             //            borderRadius: BorderRadius.circular(10)),
-             //        child: Text(
-             //          AppConstant.start,
-             //          style: TextStyle(
-             //            fontFamily: AppConstant.sansFont,
-             //            color: AppColors.whitePrimary,
-             //            fontSize: 16,
-             //            fontWeight: FontWeight.w600,
-             //          ),
-             //        ),
-             //      ),
-             //    ),
-             //  ),
+            // START BUTTON
+              InkWell(
+                onTap: () {
+                  controller.startTheGame();
+                },
+                child: Center(
+                  child: Container(
+                    height: height * 0.07,
+                    width: width * 0.8,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: AppColors.redPrimary,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text(
+                      AppConstant.start,
+                      style: TextStyle(
+                        fontFamily: AppConstant.sansFont,
+                        color: AppColors.whitePrimary,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
 
             ],
           ),
